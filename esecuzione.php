3702bd9dac025497b050377487c157aa5247463e
anto-iorfida <antonioiorfida@hotmail.com>
2024-04-30 16:19:40 +0200

    create form

---
+++ esecuzione.php
@@ -0,0 +1,17 @@
+<?php
+$name = $_GET['textUser'];
+$lastname = $_GET['parolaCensurata'];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <h2>Ciao <?php echo $name; ?> <?php echo $lastname; ?></h2>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,1 +1,26 @@
-come
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="esecuzione.php" method="GET">
+        <div>
+        <label >Text: </label><br>
+        <textarea name="textUser" type="text" ></textarea>
+        </div>
+        <br>
+        <div>
+            <label>Parola da censurare</label>
+            <input name="parolaCensurata" type="text" />
+        </div>
+        <br><br>
+        <div>
+            <button type="submit">Invia dati</button>
+        </div>
+    </form>
+</body>
+</html>
+
